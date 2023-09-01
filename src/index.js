a0fc7629f0171df184378007385ab6accb8cf5c1
Vini Costa <vinnicf@gmail.com>
2023-08-31 21:00:06 -0300

    Make add project button functional

--- src/index.js
+++ src/index.js
@@ -10,6 +10,84 @@ class Project {
 }
 
 
+
+let projects = JSON.parse(localStorage.getItem('projects')) || [];
+
+
+function addProject(name) {
+    const newProject = new Project(name);
+    projects.push(newProject);
+    localStorage.setItem('projects', JSON.stringify(projects));
+  }
+
+
+function renderProjects() {
+    const projectList = document.querySelector('.sidebar-projects');
+    projectList.innerHTML = ''; // Clear existing projects
+    projects.forEach(project => {
+      const projectItem = document.createElement('div');
+      projectItem.classList.add('sidebar-item');
+      projectItem.innerHTML = `<i class="fas fa-folder"></i><p>${project.name}</p>`;
+      projectList.appendChild(projectItem);
+    });
+  }
+  
+
+document.querySelector('.add-project').addEventListener('click', function() {
+
+    this.style.display = 'none';
+
+    //Input field
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Enter project name';
+    input.id = 'new-project-name';
+    input.classList.add('project-input');
+
+    //  'Save' button
+    const saveButton = document.createElement('button');
+    saveButton.innerHTML = 'Save';
+    saveButton.classList.add('save-project');
+
+    // 'Cancel' button
+     const cancelButton = document.createElement('button');
+    cancelButton.innerHTML = 'Cancel';
+    cancelButton.classList.add('cancel-project');
+
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.appendChild(input);
+  sidebar.appendChild(saveButton);
+  sidebar.appendChild(cancelButton);
+
+ // Event Listener for the 'Save' button
+ saveButton.addEventListener('click', () => {
+    const name = input.value;
+    if (name) {
+      addProject(name);
+      renderProjects();
+      // Remove input and buttons
+      input.remove();
+      saveButton.remove();
+      cancelButton.remove();
+      // Show the 'Add Project' button again
+      document.querySelector('.add-project').style.display = 'flex';
+    }
+  });
+
+
+// Event Listener for the 'Cancel' button
+cancelButton.addEventListener('click', () => {
+     // Remove input and buttons
+    input.remove();
+    saveButton.remove();
+    cancelButton.remove();
+        // Show the 'Add Project' button again
+        document.querySelector('.add-project').style.display = 'flex';
+      });
+
+});
+
+  
 class Task {
     constructor(title,text) {
       this.title = title
@@ -20,13 +98,8 @@ class Task {
   }
 
 
-const projects = [new Project('Home'), new Project('Work')];
 
-let tasks = [
-    { title: 'Lavar o carro', text:'O carro esta bem sujo ne', projectId: projects[0].id, id: 1693435297401 },
-    { title: 'Aprender react', text:'Precisamos disso para evoluir na vida', projectId: projects[0].id, id: 1693435298401 }
-  ];
-
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 
 // Function to render tasks
@@ -49,10 +122,10 @@ const renderTasks = (taskArray) => {
     });
   };
 
-
   // Render tasks when the page loads
 document.addEventListener('DOMContentLoaded', () => {
     renderTasks(tasks);
+    renderProjects()
  
     const addTaskForm = document.getElementById('addTaskForm');
 
@@ -70,6 +143,8 @@ document.addEventListener('DOMContentLoaded', () => {
             const newTask = new Task(taskText);
       
             tasks.push(newTask);
+
+            localStorage.setItem('tasks', JSON.stringify(tasks));
       
             renderTasks(tasks);
 
