457ed6add5ee0f4801bba2d09794e784f4930dc5
Vini Costa <vinnicf@gmail.com>
2023-09-07 11:31:24 -0300

    Change the color palette. Add icon to title.

--- src/modal.js
+++ src/modal.js
@@ -32,12 +32,12 @@ export function openModal(taskId, taskTitle) {
   };
 }
 
-// When the user clicks on <span> (x), close the modal
+// When user clicks on (x), close the modal
 btnClose.onclick = function () {
   modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
+// When user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target === modal) {
     modal.style.display = "none";
